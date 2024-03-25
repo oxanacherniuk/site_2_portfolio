24a0badf01f50ea1afee14393941911a5eec50e2
Oksana <oxanacheryssss@gmail.com>
2024-03-26 01:17:55 +0300

    ready layout

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+function menuMobile() {
+    document.querySelector('.menu').style.display='block';
+    document.querySelector('.close-btn ').style.display='block';
+    document.querySelector('.mobile-btn').style.display='none';
+}
+
+function closeMenu() {
+    document.querySelector('.menu').style.display='none';
+    document.querySelector('.close-btn ').style.display='none';
+    document.querySelector('.mobile-btn').style.display='block';
+}
